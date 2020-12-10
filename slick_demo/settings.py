935d144036ab2b477d0fc25d91575796f1aabb01
Ramez Issac <ramez@rasystems.io>
2020-12-11 00:39:51 +0200

    Settings

--- slick_demo/settings.py
+++ slick_demo/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ADMINS = (('Ramez', 'ramezashraf@gmail.com'),)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
